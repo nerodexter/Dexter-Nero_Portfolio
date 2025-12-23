3126c754c5a4c8ca13e15fdd19143431127c845b
Dexter Nero <157469134+nerodexter@users.noreply.github.com>
2025-12-23 23:49:21 +0800

    Delete dist directory

--- dist/toggle-menu.js
+++
@@ -1,6 +0,0 @@
-const button = document.getElementById('menu-button');
-const menu = document.getElementById('mobile-menu');
-
-button.addEventListener('click', () => {
-  menu.classList.toggle('hidden');
-});
