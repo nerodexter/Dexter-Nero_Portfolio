743a1d9d7afe7882aabe09f4273ce3c51b232774
Dexter Nero <157469134+nerodexter@users.noreply.github.com>
2025-12-23 23:00:58 +0800

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  content: [
+    "index.html",
+    "./src/**/*.{html,js}",
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        inter: ['Inter', 'sans-serif'],
+        adamina: ['Adamina', 'serif'],
+      },
+    },
+  },
+  plugins: [],
+}
